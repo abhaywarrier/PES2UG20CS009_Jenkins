66c0665c478e770837a4d15a6606448ca91aba79
abhaywarrier <92520441+abhaywarrier@users.noreply.github.com>
2023-02-13 22:34:46 +0530

    Create new.cpp

---
+++ new.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "I, PES2UG20CS009 Abhay R Warrier, have successfully built and run\n";
+  return 0;
+}
