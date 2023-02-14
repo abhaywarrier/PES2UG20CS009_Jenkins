b419032ff3d2d0c5f39e40021f1e42fc3cdb77b1
abhaywarrier <92520441+abhaywarrier@users.noreply.github.com>
2023-02-14 21:01:26 +0530

    Delete new.cpp

--- new.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "I, PES2UG20CS009 Abhay R Warrier, have successfully built and run\n";
-  return 0;
-}
